977bfb5ef75dd70d04fa61c0a2e05c9b20a35ce3
Ravi Shivam Autar <ravi_autar27@hotmail.com>
2015-09-07 00:14:39 +0200

    trying to add sound

--- GangstaFishyGame/src/Application.java
+++ GangstaFishyGame/src/Application.java
@@ -1,6 +1,11 @@
 import java.awt.EventQueue;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
 import Controller.Controller;
+import sun.applet.Main;
 
 /**
  * 
@@ -8,16 +13,36 @@ import Controller.Controller;
  *
  */
 
-public class Application{
+public class Application {
+
+	public static synchronized void playSound() {
+		new Thread(new Runnable() {
+			// The wrapper thread is unnecessary, unless it blocks on the
+			// Clip finishing; see comments.
+			public void run() {
+				try {
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream inputStream = AudioSystem
+							.getAudioInputStream(Main.class.getResourceAsStream("sounds/Thug Life Music.mp3"));
+					clip.open(inputStream);
+					clip.start();
+				} catch (Exception e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		}).start();
+	}
 
 	public static void main(String[] args) {
-	    EventQueue.invokeLater(new Runnable() {
-	      
-	    	@Override
-	      public void run() {
-              Controller controller = new Controller();
-//              controller.control();
-	    	}
-	    });
-	  }	
+
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				playSound();
+				Controller controller = new Controller();
+				// controller.control();
+			}
+		});
+	}
 }
