ebee94fe4fa542ef19061f0f7eafe4c8af6e214b
Ravi Shivam Autar <ravi_autar27@hotmail.com>
2015-09-07 01:04:36 +0200

    added looping soundtrack

--- GangstaFishyGame/src/Application.java
+++ GangstaFishyGame/src/Application.java
@@ -1,17 +1,14 @@
 import java.awt.EventQueue;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
-import Controller.Controller;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import sun.audio.AudioData;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
-import sun.audio.ContinuousAudioDataStream;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import Controller.Controller;
 
 /**
  * 
@@ -26,25 +23,34 @@ public class Application {
 			// The wrapper thread is unnecessary, unless it blocks on the
 			// Clip finishing; see comments.
 			public void run() {
-				AudioPlayer MGP = AudioPlayer.player;
-				AudioStream BGM;
-				AudioData MD;
 
-				ContinuousAudioDataStream loop = null;
-
+		        AudioInputStream inputStream = null;
 				try {
-					InputStream test = new FileInputStream(filename);
-					BGM = new AudioStream(test);
-					AudioPlayer.player.start(BGM);
-					// MD = BGM.getData();
-					// loop = new ContinuousAudioDataStream(MD);
-
-				} catch (FileNotFoundException e) {
-					System.out.print(e.toString());
-				} catch (IOException error) {
-					System.out.print(error.toString());
+					inputStream = AudioSystem.getAudioInputStream(new File(filename));
+				} catch (UnsupportedAudioFileException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				MGP.start(loop);
+		        Clip clip = null;
+				try {
+					clip = AudioSystem.getClip();
+				} catch (LineUnavailableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        try {
+					clip.open(inputStream);
+				} catch (LineUnavailableException | IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        clip.loop(Clip.LOOP_CONTINUOUSLY);
+		        try {
+					Thread.sleep(10000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} // looping as long as this thread is alive
 			}
 		}).start();
 	}
