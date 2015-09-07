f2bafbae5001d29ea2ee6217fcf63a8c6057f9ec
xcwfwf44 <xcwfwf44@gmail.com>
2015-09-07 06:50:20 +0200

    opmerking over de muziek functionaliteit toegevoegd, muziek functionaliteit moet deel zijn van de view

--- GangstaFishyGame/src/Application.java
+++ GangstaFishyGame/src/Application.java
@@ -36,7 +36,7 @@ public class Application {
 	}
 
 	public static synchronized void playSound(final String filename) {
-		
+		//Dit moet eigenlijk deel zijn van de View.
 		//Dit kan zonder Thread en alles mag in een try catch block als je toch alleen e.printStackTrace(); doet
 		//Als je iets anders wilt doen voor een specifieke exception, een try met multiple catch blocks.
 		
