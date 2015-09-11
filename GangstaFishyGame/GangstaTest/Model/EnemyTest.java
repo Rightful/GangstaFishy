9c76fa74f3d718c4cbf3d1ab0323ddf31891429d
humanjas <yj.hu@ziggo.nl>
2015-09-11 22:06:50 +0200

    First version EnemyTest

---
+++ GangstaFishyGame/GangstaTest/Model/EnemyTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package Model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Jasper
+ *
+ */
+public class EnemyTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Model.Enemy#loadSprites()}.
+	 */
+	@Test
+	public void testLoadSprites() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.Enemy#createEnemy()}.
+	 */
+	@Test
+	public void testCreateEnemy() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.Enemy#isToLeft()}.
+	 */
+	@Test
+	public void testIsToLeft() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.Enemy#setToLeft(boolean)}.
+	 */
+	@Test
+	public void testSetToLeft() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.Enemy#getAnimSprite()}.
+	 */
+	@Test
+	public void testGetAnimSprite() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Model.Enemy#setAnimSprite(java.awt.image.BufferedImage)}.
+	 */
+	@Test
+	public void testSetAnimSprite() {
+		fail("Not yet implemented");
+	}
+
+}
