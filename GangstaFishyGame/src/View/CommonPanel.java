2174e071292f48a928a9dac423cd32881e9241f8
tadzjibov <k.z.tadzjibov@student.tudelft.nl>
2015-09-11 15:51:57 +0200

    utf-8 arrows in isntruction

--- GangstaFishyGame/src/View/CommonPanel.java
+++ GangstaFishyGame/src/View/CommonPanel.java
@@ -106,10 +106,10 @@ public class CommonPanel extends JPanel {
 	}
 
 	public void instruction(Graphics g) {
-		g.drawString("press ↑ to move GangaFishy up", 20, viewFrame.getHeight() / 4);
-		g.drawString("press ↓ to move GangaFishy down", 20, viewFrame.getHeight() / 4+30);
-		g.drawString("press ← to move GangaFishy left", 20, viewFrame.getHeight() / 4+60);
-		g.drawString("press → to move GangaFishy right", 20, viewFrame.getHeight() / 4+90);
+		g.drawString("press \u2191 to move GangaFishy up", 20, viewFrame.getHeight() / 4);
+		g.drawString("press \u2193 to move GangaFishy down", 20, viewFrame.getHeight() / 4+30);
+		g.drawString("press \u2190 to move GangaFishy left", 20, viewFrame.getHeight() / 4+60);
+		g.drawString("press \u2192 to move GangaFishy right", 20, viewFrame.getHeight() / 4+90);
 		g.drawString("Navigate GangstaFishy through the deep dark ocean of terror and", 20, viewFrame.getHeight() / 4+120);
 		g.drawString("devour smaller fishies.", 20, viewFrame.getHeight() / 4+150);
 		g.drawString("Look out for bigger GangstaPiranhas and don't get eaten", 20, viewFrame.getHeight() / 4+180);
