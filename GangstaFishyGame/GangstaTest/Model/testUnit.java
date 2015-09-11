d42d2395ace81596c6e92595a53c4a4381076c2c
humanjas <yj.hu@ziggo.nl>
2015-09-11 22:10:58 +0200

    Added javadoc for unit test

--- GangstaFishyGame/GangstaTest/Model/testUnit.java
+++ GangstaFishyGame/GangstaTest/Model/testUnit.java
@@ -7,14 +7,25 @@ import java.awt.geom.Ellipse2D;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author Jasper
+ *
+ */
 public class testUnit {
 	Unit unit;
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
 	public void setup() {
 		unit = new Unit();
 	}
 
+	/**
+	 * Test method for {@link Model.Unit#getBoundary()}.
+	 * Test method for {@link Model.Unit#setBoundary()}.
+	 */
 	@Test
 	public void testBoundary() {
 		Ellipse2D el = new Ellipse2D.Double(2, 3, 4, 5);
@@ -22,30 +33,50 @@ public class testUnit {
 		assertEquals(el, unit.getBoundary());
 	}
 
+	/**
+	 * Test method for {@link Model.Unit#getX()}.
+	 * Test method for {@link Model.Unit#setX()}.
+	 */
 	@Test
 	public void testX() {
 		unit.setX(20);
 		assertEquals(20, unit.getX());
 	}
 
+	/**
+	 * Test method for {@link Model.Unit#getY()}.
+	 * Test method for {@link Model.Unit#setY()}.
+	 */
 	@Test
 	public void testY() {
 		unit.setY(20);
 		assertEquals(20, unit.getY());
 	}
 
+	/**
+	 * Test method for {@link Model.Unit#getWidth()}.
+	 * Test method for {@link Model.Unit#setWidth()}.
+	 */
 	@Test
 	public void testWidth() {
 		unit.setWidth(20);
 		assertEquals(20, unit.getWidth());
 	}
 
+	/**
+	 * Test method for {@link Model.Unit#getHeight()}.
+	 * Test method for {@link Model.Unit#setHeight()}.
+	 */
 	@Test
 	public void testHeight() {
 		unit.setHeight(20);
 		assertEquals(20, unit.getHeight());
 	}
 
+	/**
+	 * Test method for {@link Model.Unit#isStop()}.
+	 * Test method for {@link Model.Unit#setStop()}.
+	 */
 	@Test
 	public void testStop() {
 		unit.setStop(true);
@@ -54,18 +85,30 @@ public class testUnit {
 		assertEquals(false, unit.isStop());
 	}
 	
+	/**
+	 * Test method for {@link Model.Unit#getSprite()}.
+	 * Test method for {@link Model.Unit#setSprite()}.
+	 */
 	@Test
 	public void testSprite() {
 		unit.setSprite(null);
 		assertEquals(null, unit.getSprite());
 	}
 	
+	/**
+	 * Test method for {@link Model.Unit#getSpeed()}.
+	 * Test method for {@link Model.Unit#setSpeed()}.
+	 */
 	@Test
 	public void testSpeed() {
 		unit.setSpeed(10);
 		assertEquals(10, unit.getSpeed());
 	}
 	
+	/**
+	 * Test method for {@link Model.Unit#getRepaintTime()}.
+	 * Test method for {@link Model.Unit#setSpeed()}.
+	 */
 	@Test
 	public void testRepaintTime() {
 		unit.setSpeed(10);
