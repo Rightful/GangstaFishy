22169c04ad2cbe45c7f441f500c9c649248ac461
Faysal <f.elbouaiadi@student.tudelft.nl>
2015-09-23 16:56:05 +0200

    Fixed some bug

--- src/main/java/Model/Logger.java
+++ src/main/java/Model/Logger.java
@@ -5,7 +5,7 @@ public abstract class Logger {
 	public static final int ERROR = 1;
 	public static final int NOTICE = 2;
 	public static final int DEBUG = 3;
-	public static final String filename = "application.log";
+	public static final String filename = "data/application.log";
 	
 	/** 
 	 * Writes a prefixed/timestamped log message to a file with the name and the message
